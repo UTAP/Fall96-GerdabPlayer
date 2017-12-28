d49f2ef4ee0cffa794a11fa4bf8792e495f006f9
behzad-ost <behzad.ost@gmail.com>
2017-12-28 15:47:05 +0330

    Rename WAV Files

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,19 +1,12 @@
 #include "../src/gerdabPlayer.hpp"
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_mixer.h"
 
 int main(int argc, char* argv[]){
   GerdabPlayer player;
   player.setMusic("examples/beat.wav");
-  // Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 );
-  // Mix_PlayMusic(music, 1);
-  // Mix_Music *music = Mix_LoadMUS("examples/beat.wav");
 
-  // while(1){}
   char c;
   while (1) {
     std::cin>>c;
-    // std::cout<<player.getRemainingTime()<<std::endl;
     if(c == 'p')
       player.play();
     else if(c == 's')
@@ -23,7 +16,7 @@ int main(int argc, char* argv[]){
     else if(c == 'd')
       player.pause();
     else if(c == 'n')
-      player.setMusic("examples/behzad.wav");
+      player.setMusic("examples/book.wav");
     else
       player.pause();
   }
