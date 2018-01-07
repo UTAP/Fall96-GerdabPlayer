c78e6a890a3291d11a3ec87f48abca647e4136eb
behzad-ost <behzad.ost@gmail.com>
2018-01-07 20:10:43 +0330

    Add next and prev functions

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -8,17 +8,21 @@ using namespace std;
 int main(int argc, char* argv[]){
   string m1 = "examples/beat.wav";
   string m2 = "examples/beat.wav";
-  string m3 = "examples/beat.wav";
+  // string m1 = "examples/book.wav";
+  // string m2 = "examples/book.wav";
+  string m3 = "examples/book.wav";
+  string m4 = "examples/book.wav";
   // string m2 = "examples/book.wav";
 
   vector<string> queue;
   queue.push_back(m1);
   queue.push_back(m2);
   queue.push_back(m3);
+  queue.push_back(m4);
   GerdabPlayer player;
 
   player.setQueue(queue);
-  player.setRepeat(0);
+  player.setRepeat(1);
 
   char c;
   while (1) {
@@ -33,7 +37,9 @@ int main(int argc, char* argv[]){
     else if(c == 'd')
       player.pause();
     else if(c == 'n')
-      player.setMusic("examples/book.wav");
+      player.next();
+    else if(c == 'l')
+      player.previous();
     else
       player.pause();
   }
